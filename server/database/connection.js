f098b3e6d5b256502aaad0ad5a73a7f20ea1ab07
dh ch <chaalaadhif@gmail.com>
2023-05-01 09:59:08 +0200

    rectify adress db

--- server/database/connection.js
+++ server/database/connection.js
@@ -1,10 +1,11 @@
 const mongoose = require('mongoose')
 const databaseUrl =
-  process.env.DATABASE_URL || 'mongodb://127.0.0.1:27017/' /*'mongodb://localhost/argentBankDB'*/
+  process.env.DATABASE_URL || 'mongodb://127.0.0.1:27017/argentBankDB' /*'mongodb://localhost/argentBankDB'*/
 
 module.exports = async () => {
   try {
-    await mongoose.connect(databaseUrl, { useNewUrlParser: true })
+    await mongoose.connect(databaseUrl, { useNewUrlParser: true, useUnifiedTopology: true, 
+      useFindAndModify: false })
     console.log('Database successfully connected')
   } catch (error) {
     console.error(`Database Connectivity Error: ${error}`)
